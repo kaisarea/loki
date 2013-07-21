59ca4ceac158d63a33552f1a0f6aa71ce26d9236
Claus et. al <econ@yuno.us>
2013-07-21 23:05:15 +0000

    Nail: Designed inconstancy

--- web2py/applications/utility/models/prisoner.py
+++ web2py/applications/utility/models/prisoner.py
@@ -2,11 +2,10 @@ options.prisoner = {'price' : [.15, .30],
                     'disagreeable' : [True, False],
                     'training' : [True, False],
                     'inconstancy' : [True, False],
-                    'limit' : [True, False],
                     'improbability' : [True, False],
                     'improbability_rate' : [10, 20, 30],
                     'mystery_task': True,
-                    'work_limit' : [50, 70, 80, 90, 100]}
+                    'work_limit' : [70, 140]}
 
 
     
