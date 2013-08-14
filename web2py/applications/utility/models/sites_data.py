aee8c3ed33607dedad1ef6938022257d2f954490
kaisar <kaisarea@gmail.com>
2013-08-13 23:07:39 -0700

    Nail: Increased the quantity of images in image tagging to 300 for the control group. Still have to download around 300 for the treatment group.

--- web2py/applications/utility/models/sites_data.py
+++ web2py/applications/utility/models/sites_data.py
@@ -1,6 +1,6 @@
 # coding: utf8
 import os
-pics_treatment = [
+"""pics_treatment = [
     {"url" : "http://manteresting.com/nail/316572"},
 	{"url" : "http://25.media.tumblr.com/a473ae5b8a3f7bac080a1cc8d8860e24/tumblr_mm9601pDiR1qzerzpo1_500.jpg"},
     {"url" : "http://25.media.tumblr.com/01c37118fb5e8427a4f4ad3c226ad4b8/tumblr_mm6wdph1kF1qzerzpo1_500.jpg"},
@@ -16,19 +16,12 @@ pics_treatment = [
     {"url" : "http://www.fullpunch.com/wp-content/uploads/2013/01/disturbing-pictures-over-the-web-9.jpg"}
         
         ]
+"""
 
-list_files = os.listdir("/home/econ/loki/web2py/applications/utility/static/genova/control")
+list_files = os.listdir("/home/kaisar/loki/web2py/applications/utility/static/genova/control")
 pics_control = []
 for single_file in list_files:
   full_path = "/static/genova/control/" + single_file
   pics_control.append({"url" : full_path})
-#pics_control = [
-#    {"url" : "/static/genova/iowa-state-capitol.jpg"},
-#    {"url" : "/static/genova/sao-paulo.jpg"},
-#    {"url" : "/static/genova/beverly-hills.jpg"},
-#    {"url" : "/static/genova/granite.jpg"},
-#    {"url" : "/static/genova/watermelon.jpg"},
-#    {"url" : "/static/genova/jesus.jpg"},
-#    {"url" : "/static/genova/figs.jpg"},
-#    {"url" : "/static/genova/bike.jpg"}
-#        ]
+
+pics_treatment = pics_control
