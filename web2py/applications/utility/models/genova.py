182ef117f6a81b553bdbb9e8677a086fe7dcb56b
Claus et. al <econ@yuno.us>
2014-04-18 12:21:06 +0000

    added special condition to genova, refactored the loading of pics from filesystem

--- web2py/applications/utility/models/genova.py
+++ web2py/applications/utility/models/genova.py
@@ -1,9 +1,42 @@
-options.genova = {'price' : [.05, .10, .15, .20, .25, .30, .35, .40, .45, .50],
-		  'disagreeable' : [False, True],
+options.genova = {'price' : [.05, .10, .15, .20, .25, .30, .35, .40, .45, .50, .55, .60],
+		  'disagreeable' : [0, 50, 100],
                   'training' : [False, True],
-		  'pics_per_task': [5],
                   'improbability_rate' : [93, 56],
+                  'availability' : ['high', 'low'],
+
+		  'pics_per_task': 5,
                   'mystery_task': True,
-                  'work_limit' : [50],
+                  'work_limit' : 50,
                   'pay_delay' : 13 * 60,
-                  'first_time_bonus' : 0.50}
+                  'first_time_bonus' : 0.50,
+
+                  'special_conditions' : [(
+                       .07, {'price' : .25,
+                             'disagreeable' : False,
+                             'training' : True,
+                             'improbability_rate' : 56,
+                             'availability' : 'low'}
+                      )]
+}
+
+
+# This is how we load pictures
+def define_genova_pics():
+    """
+    For treatment we will later do analogous code as for the control
+    where directory will be crawled and all the content read
+    in. Disturbing images come from disturbingpictures.tumblr.com.  I
+    got as far as http://disturbingpictures.tumblr.com/page/22. Next
+    time continue with page 23.
+    """
+    global genova_pics
+
+    def load_pics(subdir):
+        filenames = os.listdir("applications/utility/static/genova/%s" % subdir)
+        return ['/static/genova/%s/%s' % (subdir, f) for f in filenames]
+
+    genova_pics = Storage()
+    genova_pics.control   = load_pics('control')
+    genova_pics.treatment = load_pics('treatment')
+    genova_pics.food      = load_pics('food')
+    return genova_pics
--- web2py/applications/utility/models/sites_data.py
+++ web2py/applications/utility/models/sites_data.py
@@ -1,20 +1,2 @@
 # coding: utf8
 import os
-""" For treatment we will later do analogous code as for the control where directory will be crawled
-	and all the content read in. Disturbing images come from disturbingpictures.tumblr.com.
-	I got as far as http://disturbingpictures.tumblr.com/page/22. Next time continue with
-	page 23."""
-
-list_files = os.listdir("/home/econ/loki/web2py/applications/utility/static/genova/control")
-pics_control = []
-for single_file in list_files:
-  full_path = "/static/genova/control/" + single_file
-  pics_control.append({"url" : full_path})
-
-list_files = os.listdir("/home/econ/loki/web2py/applications/utility/static/genova/treatment")
-pics_treatment = []
-for single_file in list_files:
-  full_path = "/static/genova/treatment/" + single_file
-  pics_treatment.append({"url" : full_path})
-
-#pics_treatment = pics_control
