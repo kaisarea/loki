9447d7dd56b09d6c5b8433f4bee59ee804a26400
Claus et. al <econ@yuno.us>
2014-04-20 09:31:58 +0000

    Refactored html to remove tables. Refactored validation. All javascript works.

---
+++ web2py/applications/utility/static/genova/validation.js
@@ -0,0 +1,49 @@
+function validate (event) {
+    $('.error').removeClass('error')
+	$('.error_message').hide();
+    $.post('/genova/track_error_submit', $('#response').serialize());
+    
+    // First, downcase and trim whitespace from all tags
+    $('input.keyword').each(function () {$(this).val($(this).val().toLowerCase().trim())})
+
+    // Compute the duplicates
+    var duplicates = Object();
+    var keywords = 'input.keyword';
+    //duplicates[''] = true; // So that you can't "duplicate" the empty string :)
+    //var keywords = 'input[class=keyword]';
+
+    // Go through each image's set of 5 tags
+	sets = '.image_set';
+	$(sets).each(function() {
+        var $curr_set = $(this)
+
+        // Highlight all duplicate and blank tags in this set
+		$curr_set.find(keywords).each(function() {
+            console.log('Dupes:', $(this).val(), $curr_set.find('input.keyword:text[value="'
+                                                                + $(this).val() + '"]').length)
+			has_duplicate = $curr_set.find('input.keyword:text[value="'
+                                           + $(this).val() + '"]').length > 1
+            is_empty = $(this).val() == ''
+			if(has_duplicate || is_empty)
+				$(this).parent('p').addClass('error')
+		})
+
+        // The radio button is empty if there are two unchecked radio
+        // buttons within this image set
+        var empty_radio = $curr_set.find('input[type="radio"]:not(:checked)').length > 1
+        if (empty_radio)
+            $(this).find('.radio').addClass('error')
+	})
+
+    if ($('.error').length > 0) {
+        event.preventDefault()
+	    $('p.error_message').show();
+        // And now scroll the error message into view
+        $('html, body').animate({scrollTop: $(document).height()}, 'slow');
+    }
+}
+
+
+$( function () {
+    $('#response').on('submit', validate)
+})
