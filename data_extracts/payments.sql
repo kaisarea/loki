ce11c76d7ee174aa429f974240a17d88a272cba2
kaisarea <kaisarea@gmail.com>
2015-10-11 18:59:07 +0000

    Changing names of some sql queries files so they adhere to the clean code guidelines.

--- data_extracts/payments.sql
+++
@@ -1,7 +0,0 @@
--- the point of this file is to see who was not paid
--- we have a table side_payments but the information in it is very confusing
-
-select count(*) from side_payments group by workerid
-
-;
-
