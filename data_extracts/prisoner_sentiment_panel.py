0215413acb1a4eb22874fb208548dee5c675be38
kaisarea <kaisarea@gmail.com>
2015-10-14 04:47:19 +0000

    Started reorganizing the original prisoner quality data sentiment analysis script around OOP, had to change the way I was adding the sentiment data because of the need to separate the different functions of the script. Trying to do the first in pandas as well and using map(), apply() functions to perform the addition of sentiment information to the dataset.

--- data_extracts/prisoner_sentiment_panel.py
+++ data_extracts/prisoner_sentiment_panel.py
@@ -1,15 +1,70 @@
+"""
+Transform prisoner quality data and create a panel
+
+Take data with the following schema:
+time_period,workerid,hitid,assid,submit,training_dummy,work_limit,
+disagreeable_dummy,improbability_rate,letter
+and add sentiment analysis to it
+"""
 import requests
 import json
 import csv
 import pandas
 
-#workerid,assid,hitid,time,work_limit,disagreeable,improbability_rate,training,wage,letter
-# in previous version we created pandas data set on the fly and the new data appended there
+class ExperimentData:
+    """
+    encapsulates the data to be worked with
 
-def transform_data():
-   modified_dataset = pandas.DataFrame(columns=[
+    this class will contain methods working on the data, like import, save, 
+    transform etc
+    """
+
+    def __init__(self, filename):
+        self.filename = filename
+        
+
+    def load_input(self):
+        """Load csv file into the class property for later use"""
+        self.dataframe = pandas.read_csv(self.filename)
+       
+    def add_sentiment(self):
+        """call evaluate_sentiment() method for every row in the dataset"""
+        letter_series = self.dataframe.letter        
+        sentiment_data = letter_series.apply(self._evaluate_sentiment)
+        self.dataframe['sentiment'] = sentiment_data
+     
+    def _unpack_sentiment_data(self):
+        """break up the sentiment data into four columns within the dataset"""
+        self.dataframe.apply( 
+            'negative_sentiment': letter_data['probability']['neg'],
+            'positive_sentiment': letter_data['probability']['pos'],
+            'neutral_sentiment': letter_data['probability']['neutral'],
+            'label': letter_data['label']})
+
+    def _evaluate_sentiment(text):
+        """uses the API to determine a score for sentiment"""
+        payload = {'text': text}
+        r = requests.post("http://text-processing.com/api/sentiment/", data=payload)
+        #print(r.status_code)
+        sentiment_data = json.loads(r.text)
+        #sentiment_data_prob = sentiment_data['probability']
+
+        #neg, neutral, pos
+        #negative_sentiment = sentiment_data_prob['neg']
+        #positive_sentiment = sentiment_data_prob['pos']
+        #neutral_sentiment = sentiment_data_prob['neutral']
+        #sentiment_label = sentiment_data['label']
+        return sentiment_data
+
+    def add_word_count(self):
+
+    def add_unique_word_count(self):
+
+    def save_data(self):
+        modified_dataset = pandas.DataFrame(columns=
+        [   # list is an ordered collection 
+            'time_period',
       'workerid',
-      'assid',
       'hitid',
       'time',
       'work_limit',
@@ -21,54 +76,29 @@ def transform_data():
       'positive_sentiment',
       'neutral_sentiment',
       'label'])
-
-   with open('/home/econ/sql/data_extract_prisoner_quality.csv') as csv_source:
-      prisoner_reader = csv.reader(csv_source, delimiter=',')
-      index = 0
-      for row in prisoner_reader:
-         index += 1
-         print(index)
-         if index == 1:
-            continue
-         #print(len(row))
-         letter_from_worker = row[9]
-         letter_data = evaluate_sentiment(letter_from_worker)
-         #print(letter_data['probability']['pos'])
-         modified_dataset.loc[index-1] = pandas.Series({
-            'workerid': row[0],
-            'assid': row[1],
-            'hitid': row[2],
-            'time': row[3],
-            'work_limit': row[4],
-            'disagreeable': row[5],
-            'success_rate': row[6],
-            'test': row[7],
-            'wage': row[8],
-            'negative_sentiment': letter_data['probability']['neg'],
-            'positive_sentiment': letter_data['probability']['pos'],
-            'neutral_sentiment': letter_data['probability']['neutral'],
-            'label': letter_data['label']})
-         if index > 5000:
-            #print(modified_dataset)
-            break
-      modified_dataset.to_csv('/home/econ/data/prisoner_quality_sentiment.csv', 
+        modified_dataset.to_csv('/home/econ/data/prisoner_quality_sentiment.csv', 
          sep=',', 
          encoding='utf-8')
 
-def evaluate_sentiment(text):
-   payload = {'text': text}
 
-   r = requests.post("http://text-processing.com/api/sentiment/", data=payload)
-   print(r.status_code)
-   sentiment_data = json.loads(r.text)
-   sentiment_data_prob = sentiment_data['probability']
+filename_ = '/home/econ/data/prisoner_quality_panel_data.csv'
+#schema_ = ['time_period',
+#               'workerid',
+#               'hitid',
+#               'assid',
+#               'submit',
+#               'training_dummy',
+#               'work_limit',
+#               'disagreeable_dummy',
+#               'improbability_rate',
+#               'letter']
+prisoner_quality = ExperimentData(filename_)
+prisoner_quality.load_input()
+prisoner_quality.add_sentiment()
+prisoner_quality.add_word_count()
+prisoner_quality.add_unique_word_count()
 
-   #neg, neutral, pos
-   negative_sentiment = sentiment_data_prob['neg']
-   positive_sentiment = sentiment_data_prob['pos']
-   neutral_sentiment = sentiment_data_prob['neutral']
-   sentiment_label = sentiment_data['label']
-   return sentiment_data
+
+   
 
 
-transform_data()
